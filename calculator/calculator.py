46f193b148c651925ee30873f0bb351c1113fd02
Brendan Kearney <bkearney@binghamton.edu>
2024-10-10 13:00:20 -0400

    calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -7,14 +7,13 @@ pygame.display.set_caption("Calculator")
 gray = (65,65,65)
 font = pygame.font.SysFont('Arial', 40, bold = False)
 
+#buttons
 zerosurf = font.render('0', True, 'white')
-zerobutton = pygame.Rect(0,500,100,100)
+zerobutton = pygame.Rect(100,500,100,100)
+onesurf = font.render('1', True, 'white')
+onebutton = pygame.Rect(0,400,100,100)
 
-def makebutton(button,x,y):
-    (button) + 'surf' = font.render(button, True, 'white')
-    (button) + 'button' = pygame.Rect()
-
-
+#function for color change when over button
 def isonbutton(button, surf):
     if (button).collidepoint((a, b)):
         pygame.draw.rect(screen, (180,180,180), button)
@@ -33,9 +32,12 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if zerobutton.collidepoint(event.pos):
                 pygame.quit()
+            if onebutton.collidepoint(event.pos):
+                pygame.quit()
     a,b = pygame.mouse.get_pos()
     isonbutton(zerobutton, zerosurf)
-
+    isonbutton(onebutton, onesurf)
+    
     pygame.display.update()
 
 pygame.quit()
